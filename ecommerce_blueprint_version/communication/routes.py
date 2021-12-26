16033d1d954531aec2eb1b16e831381efa3bdd21
Zuck Xu <ziangxuu@gmail.com>
2021-12-26 10:06:19 -0500

    finish phone verification feature, add abuse prevention

--- ecommerce_blueprint_version/communication/routes.py
+++ ecommerce_blueprint_version/communication/routes.py
@@ -1,6 +1,7 @@
 from flask import current_app, url_for, redirect, request, Blueprint, jsonify
 from twilio.rest import Client
 from datetime import datetime
+import random
 
 from ecommerce_blueprint_version import db
 from ecommerce_blueprint_version.auth.utils import *
@@ -8,12 +9,11 @@ from ecommerce_blueprint_version.auth.utils import *
 
 communication = Blueprint('communication', __name__)
 
-# Need to handle the request abuse
-@communication.route("/sendCode", methods=['POST'])
+@communication.route("/sendPhoneVerificationCode", methods=['POST'])
 @token_required
 def send_code(current_user):
     target_phone, message = request.get_json().get("phone"), request.get_json().get("message")
-    print('info', target_phone, message)
+    # print('info', target_phone, message)
 
     account_sid = current_app.config['TWILIO_ACCOUNT_SID'] 
     auth_token = current_app.config['TWILIO_AUTH_TOKEN']
@@ -26,6 +26,13 @@ def send_code(current_user):
     #                         )
 
 
-    User.query.filter_by(userid=current_user.userid).update({"phone_verification_code": 332211, "phone_verification_code_sent_time": datetime.utcnow()})
+    # Prevent someone from abusing api
+    if current_user.phone_verification_code_sent_time:
+        request_interval = int((datetime.utcnow() - current_user.phone_verification_code_sent_time).total_seconds())
+        if request_interval < 60:
+            return jsonify({'next_request_waiting_time': 60 - request_interval}), 400
+
+    generated_code = random.randrange(100000, 1000000, 2)
+    User.query.filter_by(userid=current_user.userid).update({"phone_verification_code": generated_code, "phone_verification_code_sent_time": datetime.utcnow()})
     db.session.commit()
     return "code_sent", 200
