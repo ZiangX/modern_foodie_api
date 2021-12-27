14f233fd1720507e8d1f020f0f2cb849e27034d9
Zuck Xu <ziangxuu@gmail.com>
2021-12-26 19:18:58 -0500

    finish phone verification

--- ecommerce_blueprint_version/communication/routes.py
+++ ecommerce_blueprint_version/communication/routes.py
@@ -9,30 +9,43 @@ from ecommerce_blueprint_version.auth.utils import *
 
 communication = Blueprint('communication', __name__)
 
-@communication.route("/sendPhoneVerificationCode", methods=['POST'])
+@communication.route("/phoneNumberVerification", methods=['POST', 'GET'])
 @token_required
 def send_code(current_user):
-    target_phone, message = request.get_json().get("phone"), request.get_json().get("message")
-    # print('info', target_phone, message)
+    if request.method == 'GET':
+        target_phone, message = request.get_json().get("phone"), request.get_json().get("message")
+        # print('info', target_phone, message)
 
-    account_sid = current_app.config['TWILIO_ACCOUNT_SID'] 
-    auth_token = current_app.config['TWILIO_AUTH_TOKEN']
-    client = Client(account_sid, auth_token) 
-    
-    # message = client.messages.create(  
-    #                             messaging_service_sid=current_app.config['MESSAGING_SERVICE_SID'], 
-    #                             body=message,      
-    #                             to=target_phone 
-    #                         )
+        account_sid = current_app.config['TWILIO_ACCOUNT_SID'] 
+        auth_token = current_app.config['TWILIO_AUTH_TOKEN']
+        client = Client(account_sid, auth_token) 
+        
+        # message = client.messages.create(  
+        #                             messaging_service_sid=current_app.config['MESSAGING_SERVICE_SID'], 
+        #                             body=message,      
+        #                             to=target_phone 
+        #                         )
 
 
-    # Prevent someone from abusing api
-    if current_user.phone_verification_code_sent_time:
-        request_interval = int((datetime.utcnow() - current_user.phone_verification_code_sent_time).total_seconds())
-        if request_interval < 60:
-            return jsonify({'next_request_waiting_time': 60 - request_interval}), 400
+        # Prevent someone from abusing api
+        if current_user.phone_verification_code_sent_time:
+            request_interval = int((datetime.utcnow() - current_user.phone_verification_code_sent_time).total_seconds())
+            if request_interval < 60:
+                return jsonify({'next_request_waiting_time': 60 - request_interval}), 400
 
-    generated_code = random.randrange(100000, 1000000, 2)
-    User.query.filter_by(userid=current_user.userid).update({"phone_verification_code": generated_code, "phone_verification_code_sent_time": datetime.utcnow()})
-    db.session.commit()
-    return "code_sent", 200
+        generated_code = random.randrange(100000, 1000000, 2)
+        User.query.filter_by(userid=current_user.userid).update({"phone_verification_code": generated_code, "phone_verification_code_sent_time": datetime.utcnow()})
+        db.session.commit()
+        return "code_sent", 200
+    else:
+        code = request.get_json().get("code")
+        try:
+            code = code.replace(" ", "")
+            code = int(code)
+        except:
+            return "code_malformed", 400
+
+        if current_user.phone_verification_code == code:
+            return "code_correct", 200
+        return "code_incorrect", 400
+
